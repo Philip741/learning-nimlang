354bc1fdab377b55bd041ae209758a4390a1e5d8
Philip <philip741@gmail.com>
2023-04-01 12:11:54 -0400

    Cleaned up testing code and check for proper url

--- nim-webscraper/src/nim_webscraper.nim
+++ nim-webscraper/src/nim_webscraper.nim
@@ -11,43 +11,26 @@ proc downloadPageContent(url: string): string =
 
 proc main() =
   var args = commandLineParams()
-  #var url = args[0]
+  var url = ""
   var matched = false
-  var urlText = "https://google.com"
-  #let urlPattern = re(r"^https?://([a-z0-9-]+\.)+[a-z]{2,}(\/[^\s]*)?$")
-  #match urlText, rex"^https?://([a-z0-9-]+\.)+[a-z]{2,}(\/[^\s]*)?$":
-      #echo "matched it's a url"
-      
+  
   if args.len == 1:
-    let url = args[0]
-    echo url
+    url = args[0]
     match url, rex"^https?://([a-z0-9-]+\.)+[a-z]{2,}(\/[^\s]*)?$":
       matched = true
-      if matched:
-        let htmlContent = downloadPageContent(url)
-        echo htmlContent
-      else:
-        echo "Not a valid url use http or https://url.com"
+      
 
   elif args.len == 0:
     echo args.len
-    
-  # elif args.len == 1 and args[1] == "-h":
-  #   echo "Use ./nim_webscraper <url>"
-  # elif args.len == 1:
-  #     let url = args[0]
-  #     echo url
-  # Do something with the URL here
   else:
       echo "Error: too many arguments"
-  #else:
-   # echo "Invalid option"
+ 
   
+  if matched:
+      let htmlContent = downloadPageContent(url)
+      echo htmlContent
+  elif matched == false and args.len > 0:
+      echo "Not a valid url use http or https://url.com"
   
-  #let url = "https://github.com/ansible/awx/blob/devel/INSTALL.md"
-  
-  
-  #let htmlContent = downloadPageContent(url)
-  #echo htmlContent
 when isMainModule:
   main()
